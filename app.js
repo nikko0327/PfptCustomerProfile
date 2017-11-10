e7339866feaa90c62234f7b692a08da737a5b19d
Nikko Lee <nlee@C02SGG6KG8WP.corp.proofpoint.com>
2017-11-09 16:34:24 -0800

    App configuration

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("bady-parser");
+var methodOverride = require("method-override");
+var sanitizer = require("express-sanitizer");
+var session = require("express-session");
+var MongoStore = require("connect-mongo")(session);
 var app = express();
 
+
+//APP CONFIGURATION
+
+var databaseUrl = "mongodb://localhost/customerProfile";
+mongoose.connect(databaseUrl);
+
+// set so we dont have to type .ejs all the time when routing
 app.set("view engine", "ejs");
+// used so we can make a public folder that contains stylesheet and js, and be able to access it
 app.use(express.static(__dirname + "/public"));
 //including routes. Seperating the routes to different file, so it will be cleaner.
 var routes = require("./routes/router");
+// used so we can get data from forms and etc.
+app.use(bodyParser.urlencoded({extended: true}));
+// security. This line of code has to be always after body-parser
+app.use(expressSanitizer());
+// so we can use PUT request
+app.use(methodOverride("_method"));
 app.use(routes);
 
 
--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 // var User = require("../models/user");
+// var Customer = require("../models/customer");
 
 //Entry point for the app startup
 router.get("/", function(req, res){
