62d0bf23fbd87f4da1f79033b7bb466b003ebdf2
Nikko Lee <nlee@C02SGG6KG8WP.corp.proofpoint.com>
2017-11-13 17:07:27 -0800

    Basic db structure for customer. General info only.

--- models/customer.js
+++ models/customer.js
@@ -1,7 +1,16 @@
 var mongoose = require("mongoose");
 
 var CustomerSchema = new mongoose.Schema({
-	
+	name: String,
+	status: String,
+	salesRep: String,
+	archivingSe: String,
+	accManager: String,
+	location: String,
+	supervision: String,
+	natIp: String,
+	contacts: String,
+	created: {type: Date, default: Date.now}
 });
 
 var Customer = mongoose.model("Cusotmer", CustomerSchema);
