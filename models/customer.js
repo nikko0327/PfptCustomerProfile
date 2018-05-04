998f51230c9f964c49afa4723f7e76e9c6e61d00
Nikko Lee <nlee@C02SGG6KG8WP.corp.proofpoint.com>
2018-05-03 20:47:22 -0700

    Database change

--- models/customer.js
+++ models/customer.js
@@ -2,14 +2,19 @@ var mongoose = require("mongoose");
 
 var CustomerSchema = new mongoose.Schema({
 	name: String,
-	status: String,
 	salesRep: String,
 	archivingSe: String,
 	accManager: String,
+	tem: String,
+	tam: String,
+	impSpecialist: String,
+	numberOfUsers: Integer,
 	location: String,
 	supervision: String,
-	natIp: String,
 	contacts: String,
+	status: String,
+	natIp: String,
+	incumbentSolution: String,
 	created: {type: Date, default: Date.now}
 });
 
--- routes/router.js
+++ routes/router.js
@@ -7,14 +7,19 @@ var mongoose = require("mongoose");
 
 var CustomerSchema = new mongoose.Schema({
 	name: String,
-	status: String,
 	salesRep: String,
 	archivingSe: String,
 	accManager: String,
+	tem: String,
+	tam: String,
+	impSpecialist: String,
+	numberOfUsers: String,
 	location: String,
 	supervision: String,
-	natIp: String,
 	contacts: String,
+	status: String,
+	natIp: String,
+	incumbentSolution: String,
 	created: {type: Date, default: Date.now}
 });
 
@@ -31,6 +36,23 @@ var Customer = mongoose.model("Customer", CustomerSchema);
 // 	natIp: "6Six",
 // 	contacts: "7Seven"
 // });
+
+ // Customer.create({
+	// name: "Maine Health",
+	// salesRep: "TJ Lapore",
+	// archivingSe: "Jason Colvin",
+	// accManager: "Jeffrey Heller",
+	// tem: "Aimee Wheelon",
+	// tam: "John Doe",
+	// impSpecialist: "Jane Doe",
+	// numberOfUsers: "20050",
+	// location: "Portland, ME",
+	// supervision: "No",
+	// contacts: "Paul Caron Manager, Platform Services, caronp@mainehealth.org, 207-662-6666",
+	// status: "Implementation",
+	// natIp: "63.247.60.141",
+	// incumbentSolution: "EV",
+ // });
 
 //Entry point for the app startup
 router.get("/", function(req, res){
